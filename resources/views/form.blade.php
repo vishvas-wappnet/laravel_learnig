22b2ddb7bab4d079892721b30b879d90c70dddd1
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-16 13:10:44 +0530

    user registration with validation functionality added

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,13 +8,31 @@
                         <form  actoin="http://localhost/display" method="post"> 
 
                         @csrf
-                                name:<input type="text" name="user_name"> </br>
+                            <div>
+                                name:<input type="text" name="user_name"> 
+                                <span class"error">
+                                    @error('user_name')
+                                            {{$message}}
+                                    @enderror
+                                 </span>
+                                 </br>
+                        </div>
+                                email:<input type="email" name="user_email"> 
+                                <span class"error">
+                                    @error('user_email')
+                                            {{$message}}
+                                    @enderror
+                                 </span>
+                                 </br>
 
-                                email:<input type="email" name="user_email"> </br>
-
-                                password: <input type="password" name="user_password"> </br>
-
-                                <input type="submit" value="submit"  name="btn_submit">
+                                password: <input type="password" name="user_password"> 
+                                <span class"error">
+                                    @error('user_password')
+                                            {{$message}}
+                                    @enderror
+                                 </span>
+                        </br>
+                                <input type="submit" value="submit"  name="btn_submit" style="size: 100px;">
 
                         </form>
 
