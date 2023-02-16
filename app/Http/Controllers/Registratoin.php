44f1b8ed45ea4ae4961b3cdadbc7baf751cb824c
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-02-16 12:39:56 +0530

    user registration with validators functionality added

--- app/Http/Controllers/Registratoin.php
+++ app/Http/Controllers/Registratoin.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+
+
+
+use Illuminate\Validation\Rule; 
 
 class Registratoin extends Controller
 {
@@ -13,6 +18,14 @@ class Registratoin extends Controller
 
     public function user_register(Request $request)
         {
+                $request->validate( 
+                    [
+                        'user_name' => 'required' , 
+                        'user_email' => 'required|email',
+                        'user_password'=> 'required' 
+                    ]
+                );
+
                 echo "<pre>";
                 print_r($request->all());
         }    
